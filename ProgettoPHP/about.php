f942cf5b4a0a78afdeb7a8a265628e91372b98b1
Edoardo Pappagallo <epappagallo@studenti.apuliadigitalmaker.it>
2025-07-16 10:24:07 +0200

    FolderPrincipale

---
+++ ProgettoPHP/about.php
@@ -0,0 +1,3 @@
+<?php
+echo '<h1>Chi siamo</h1><p>Questa è la pagina About.</p>';
+?> 
---
+++ ProgettoPHP/router.php
@@ -0,0 +1,17 @@
+<?php
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($uri) {
+    case '/':
+        $pagina = 'home.php';
+        break;
+    case '/about':
+        $pagina = 'about.php';
+        break;
+    default:
+        $pagina = '404.php';
+        break;
+}
+
+include $pagina;
+?> 
